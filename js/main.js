a4b0af542891c3078a7e55d84658ca25166950e3
Richard-Limbada <richard.limbada@gmail.com>
2017-09-28 02:37:26 +0200

    Update main.js

--- js/main.js
+++ js/main.js
@@ -44,20 +44,45 @@ var templates = [
 
 
 
+//aggression agility ballcontrol foot crossing dribbling finishing freekickaccuracy headingaccuracy longshots penalties shortpassing shotpower sprintspeed standingtackle stamina strength vision volleys
+
+
 var app = new Vue({
     el: '#app',
     data: {
-        tabledata: tabledata,
+        tableData2: tableData2,
         templates: templates,
         current_template: templates[0],
         default_template: 1,
         current_player_name: '',
-        current_player_name: '',
+        current_player_url: '',
+        current_player_age: '',
+        current_player_acceleration: '',
+        current_player_aggression: '',
+        current_player_agility: '',
+        current_player_ballcontrol: '',
+        current_player_foot: '',
+        current_player_crossing: '',
+        current_player_dribbling: '',
+        current_player_finishing: '',
+        current_player_freekickaccuracy: '',
+        current_player_headingaccuracy: '',
+        current_player_longshots: '',
+        current_player_penalties: '',
+        current_player_shortpassing: '',
+        current_player_shotpower: '',
+        current_player_sprintspeed: '',
+        current_player_standingtackle: '',
+        current_player_stamina: '',
+        current_player_strength: '',
+        current_player_vision: '',
+        current_player_volleys: '',
+
     },
     methods: {
-        tableHeader: function (tableData) {
+        tableHeader: function (tableData2) {
             var keys = [];
-            for (key in tableData[0]) {
+            for (key in tableData2[0]) {
                 keys.push(key);
             }
             return keys;
@@ -69,20 +94,39 @@ var app = new Vue({
 
             this.current_template = selected_template;
         },
-        pullPlayer: function (tabledata, name) {
+        pullPlayer: function (tableData2, name) {
 
-            var player = tabledata.filter(function (ele) {
-                return (ele.Name == name)
+            var player = tableData2.filter(function (ele) {
+                return (ele.name == name)
             })[0];
-            console.log(player.Name);
-            this.current_player_name = player.Name;
+            console.log(player.name);
+            this.current_player_name = player.name;
+            this.current_player_url = player.headshotImgUrl;
+            this.current_player_name = player.name;
+            this.current_player_age = player.age;
+            this.current_player_acceleration = player.acceleration;
+            this.current_player_aggression = player.aggression;
+            this.current_player_agility = player.agility;
+            this.current_player_ballcontrol = player.ballcontrol;
+            this.current_player_foot = player.foot;
+            this.current_player_crossing = player.crossing;
+            this.current_player_dribbling = player.dribbling;
+            this.current_player_finishing = player.finishing;
+            this.current_player_freekickaccuracy = player.freekickaccuracy;
+            this.current_player_headingaccuracy = player.freekickaccuracy;
+            this.current_player_longshots = player.longshots;
+            this.current_player_penalties = player.penalties;
+            this.current_player_shortpassing = player.shortpassing;
+            this.current_player_shotpower = player.shotpower;
+            this.current_player_sprintspeed = player.sprintspeed;
+            this.current_player_standingtackle = player.standingtackle;
+            this.current_player_stamina = player.stamina;
+            this.current_player_strength = player.strength;
+            this.current_player_vision = player.vision;
+
             return player
+        },
 
-
-
-        }
 
     }
 })
-
-
